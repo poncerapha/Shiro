abe13d9ce028d10c06f64e818dea1556ab437f71
Raphael Ponce <raphafleury123@gmail.com>
2020-02-07 22:35:29 -0300

    Criando cena do menu

---
+++ Shiro/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+
+    public string sceneToLoad;
+
+
+    public void LoadScene()
+    {
+        Debug.Log("Loading scene: " + sceneToLoad);
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game...");
+        Application.Quit();
+    }
+}
