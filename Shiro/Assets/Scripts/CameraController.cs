d9f16735271f94eff5a0c72a7193818154b79575
Raphael Ponce <raphafleury123@gmail.com>
2020-02-10 21:27:38 -0300

    Movimentacao do jogador e da camera

---
+++ Shiro/Assets/Scripts/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject target;
+    public Tilemap tileMap;
+
+    private Vector3 bottomLeftLimit;
+    private Vector3 topRightLimit;
+    private float halfHeight;
+    private float halfWidth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        halfHeight = Camera.main.orthographicSize;
+        halfWidth = halfHeight * Camera.main.aspect;
+
+        bottomLeftLimit = tileMap.localBounds.min + new Vector3(halfWidth, halfHeight, 0f);
+        topRightLimit = tileMap.localBounds.max + new Vector3(-halfWidth, -halfHeight, 0f);
+
+        PlayerMovement.instance.SetBounds(tileMap.localBounds.min, tileMap.localBounds.max);
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        this.transform.position = new Vector3(target.transform.position.x, target.transform.position.y, this.transform.position.z);
+
+        this.transform.position = new Vector3(Mathf.Clamp(this.transform.position.x, bottomLeftLimit.x, topRightLimit.x), Mathf.Clamp(this.transform.position.y, bottomLeftLimit.y, topRightLimit.y), this.transform.position.z);
+    }
+}
---
+++ Shiro/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D playerRB;
+    private Vector3 bottomLeftLimit;
+    private Vector3 topRightLimit;
+
+    public float moveSpeed;
+    public static PlayerMovement instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+        playerRB = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        this.transform.position = new Vector3(Mathf.Clamp(this.transform.position.x, bottomLeftLimit.x, topRightLimit.x), Mathf.Clamp(this.transform.position.y, bottomLeftLimit.y, topRightLimit.y), this.transform.position.z);
+    }
+
+    private void Move()
+    {
+        var verticalInput = Input.GetAxisRaw("Vertical");
+        var horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        playerRB.velocity = new Vector2(horizontalInput, verticalInput) * moveSpeed;
+
+    }
+
+    public void SetBounds(Vector3 botLeft, Vector3 topRight)
+    {
+        bottomLeftLimit = botLeft + new Vector3(0.5f, 1f, 0f);
+        topRightLimit = topRight + new Vector3(-0.5f, -1f, 0f);
+    }
+}
