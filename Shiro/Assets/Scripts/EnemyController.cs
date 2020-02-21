c912822bc470d15f56224cc41b093e94f031d7af
Raphael Ponce <raphafleury123@gmail.com>
2020-02-21 02:27:43 -0300

    Movimentação inimigo

---
+++ Shiro/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+
+    public float speed;
+
+    private Transform target;
+
+    private void Awake()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        if(Vector2.Distance(this.transform.position,target.position) > 3)
+        {
+            this.transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+        
+    }
+}
